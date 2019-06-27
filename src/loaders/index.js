6998fe1dce3aca393f856497fec20afa7e95fe0e
Pablo Vinicius <123amizadepablo@gmail.com>
2019-06-27 17:29:17 -0300

    imports

---
+++ src/loaders/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    Managers: require("./BotManagers.js"),
+    EventsLoaders: require("./EventsLoaders.js"),
+    CommandsLoader: require("./CommandsLoader.js"),
+    LoaderMusicUtils: require("./LoaderMusicUtils.js")
+}
---
+++ src/music/listens/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    Queue: require("./Queue.js"),
+    Guild: require("./Guild.js")
+}
