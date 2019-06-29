8e2110ea0f20b2099863d22b795b21821c60c8ec
Pablo Vinicius <123amizadepablo@gmail.com>
2019-06-29 18:20:30 -0300

    fix command

--- src/commands/music/clear.js
+++ src/commands/music/clear.js
@@ -9,24 +9,26 @@ module.exports = class Clear extends Command {
     }
 
     async run({ channel, guild, author }) {
-        const embed = new ClientEmbed(author);
-        const guildQueue = await this.client.music.module.queue.get(guild.id);
-        if (guildQueue && guildQueue.songs.length) {
-            guildQueue.clearQueue();
-            return channel.send(embed
-                .setTitle('Queue limpa com sucesso!')
-            )
-        } else {
-            if (guildQueue) {
+        const trueResult = await this.verifyVoice(guild, channel, author, voiceChannel);
+        if (trueResult) {
+            const embed = new ClientEmbed(author);
+            const guildQueue = await this.client.music.module.queue.get(guild.id);
+            if (guildQueue && guildQueue.songs.length) {
                 return channel.send(embed
-                    .setTitle('Não há nehuma música depois da atual!')
-                    .setColor(process.env.ERR_COLOR)
-                )
+                    .setTitle('Queue limpa com sucesso!')
+                ).then(() => guildQueue.clearQueue());
             } else {
-                return channel.send(embed
-                    .setTitle('Não estou tocando nada no **momento**')
-                    .setColor(process.env.ERR_COLOR)
-                )
+                if (guildQueue) {
+                    return channel.send(embed
+                        .setTitle('Não há nehuma música depois da atual!')
+                        .setColor(process.env.ERR_COLOR)
+                    )
+                } else {
+                    return channel.send(embed
+                        .setTitle('Não estou tocando nada no **momento**')
+                        .setColor(process.env.ERR_COLOR)
+                    )
+                }
             }
         }
     }
