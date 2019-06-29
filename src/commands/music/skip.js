8bb8265b7a326000a69639f4576a2bba8763cbbc
Pablo Vinicius <123amizadepablo@gmail.com>
2019-06-29 18:21:59 -0300

    fix

--- src/commands/music/skip.js
+++ src/commands/music/skip.js
@@ -10,23 +10,23 @@ module.exports = class Skip extends Command {
 
     async run({ voiceChannel, message, channel, guild, author }) {
         const trueResult = await this.verifyVoice(guild, channel, author, voiceChannel);
-        if (!trueResult) return;
-
-        const embed = new ClientEmbed(author);
-        const guildQueue = await this.client.music.module.queue.get(guild.id);
-        if (guildQueue && guildQueue.songs.length > 0) {
-            return message.react('⏩').then(() => guildQueue.skip());
-        } else {
-            if (guildQueue) {
-                return channel.send(embed
-                    .setTitle('Não há nehuma música depois dessa!')
-                    .setColor(process.env.ERR_COLOR)
-                )
+        if (trueResult) {
+            const embed = new ClientEmbed(author);
+            const guildQueue = await this.client.music.module.queue.get(guild.id);
+            if (guildQueue && guildQueue.songs.length) {
+                return message.react('⏩').then(() => guildQueue.skip());
             } else {
-                return channel.send(embed
-                    .setTitle('Não estou tocando nada no **momento**')
-                    .setColor(process.env.ERR_COLOR)
-                )
+                if (guildQueue) {
+                    return channel.send(embed
+                        .setTitle('Não há nehuma música depois dessa!')
+                        .setColor(process.env.ERR_COLOR)
+                    )
+                } else {
+                    return channel.send(embed
+                        .setTitle('Não estou tocando nada no **momento**')
+                        .setColor(process.env.ERR_COLOR)
+                    )
+                }
             }
         }
     }
