2a0723ec52cfffb0540cd69b5f14082a8d5457a8
Pablo Vinicius <123amizadepablo@gmail.com>
2019-06-29 18:22:11 -0300

    bug fix and updated

--- src/commands/music/stop.js
+++ src/commands/music/stop.js
@@ -10,20 +10,19 @@ module.exports = class Stop extends Command {
 
     async run({ voiceChannel, channel, guild, author }) {
         const trueResult = await this.verifyVoice(guild, channel, author, voiceChannel);
-        if (!trueResult) return;
-
-        const embed = new ClientEmbed(author);
-        const guildQueue = await this.client.music.module.queue.get(guild.id);
-        if (guildQueue && guildQueue.songPlaying) {
-            guildQueue.stop();
-            return channel.send(embed
-                .setTitle('A queue foi finalizada!')
-            );
-        } else {
-            return channel.send(embed
-                .setTitle('Não estou tocando nada no **momento**')
-                .setColor(process.env.ERR_COLOR)
-            )
+        if (trueResult) {
+            const embed = new ClientEmbed(author);
+            const guildQueue = await this.client.music.module.queue.get(guild.id);
+            if (guildQueue && guildQueue.songPlaying) {
+                return channel.send(embed
+                    .setTitle('A queue foi finalizada!')
+                ).then(() => guildQueue.stop());
+            } else {
+                return channel.send(embed
+                    .setTitle('Não estou tocando nada no **momento**')
+                    .setColor(process.env.ERR_COLOR)
+                )
+            }
         }
     }
 }
