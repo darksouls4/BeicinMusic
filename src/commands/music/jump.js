abf50ed572c64d9f1d899dc04177309d5f0f1b8e
Pablo Vinicius <123amizadepablo@gmail.com>
2019-06-29 18:20:39 -0300

    fix

--- src/commands/music/jump.js
+++ src/commands/music/jump.js
@@ -8,39 +8,42 @@ module.exports = class Jump extends Command {
         })
     }
 
-    async run({ message, args, channel, guild, author }) {
-        const embed = new ClientEmbed(author);
-        const guildQueue = await this.client.music.module.queue.get(guild.id);
-        if (guildQueue && guildQueue.songs.length) {
-            if (args[0]) {
-                let jump = Number(args[0]);
-                if (!(!isNaN(Number(args[0])))) return channel.send(embed
-                    .setTitle(`Por favor insira um valor numérico! de **1 á ${guildQueue.songs.length}**`)
-                    .setColor(process.env.ERR_COLOR)
-                );
-                jump = Math.round(jump);
-                if (jump > guildQueue.songs.length || jump < 1) return channel.send(embed
-                    .setTitle(`Por favor insira um número de **1 á ${guildQueue.songs.length}**!`)
-                    .setColor(process.env.ERR_COLOR)
-                );
-                return message.react('⤴').then(() => guildQueue.jump(jump));
+    async run({ voiceChannel,message, args, channel, guild, author }) {
+        const trueResult = await this.verifyVoice(guild, channel, author, voiceChannel);
+        if (trueResult) {
+            const embed = new ClientEmbed(author);
+            const guildQueue = await this.client.music.module.queue.get(guild.id);
+            if (guildQueue && guildQueue.songs.length) {
+                if (args[0]) {
+                    let jump = Number(args[0]);
+                    if (!(!isNaN(Number(args[0])))) return channel.send(embed
+                        .setTitle(`Por favor insira um valor numérico! de **1 á ${guildQueue.songs.length}**`)
+                        .setColor(process.env.ERR_COLOR)
+                    );
+                    jump = Math.round(jump);
+                    if (jump > guildQueue.songs.length || jump < 1) return channel.send(embed
+                        .setTitle(`Por favor insira um número de **1 á ${guildQueue.songs.length}**!`)
+                        .setColor(process.env.ERR_COLOR)
+                    );
+                    return message.react('⤴').then(() => guildQueue.jump(jump));
+                } else {
+                    return channel.send(embed
+                        .setTitle(`Por favor insira o numero da música para eu pular! **[1 á ${guildQueue.songs.length}]**`)
+                        .setColor(process.env.ERR_COLOR)
+                    )
+                }
             } else {
-                return channel.send(embed
-                    .setTitle(`Por favor insira o numero da música para eu pular! **[1 á ${guildQueue.songs.length}]**`)
-                    .setColor(process.env.ERR_COLOR)
-                )
-            }
-        } else {
-            if (guildQueue) {
-                return channel.send(embed
-                    .setTitle('Não há nehuma música depois da atual!')
-                    .setColor(process.env.ERR_COLOR)
-                )
-            } else {
-                return channel.send(embed
-                    .setTitle('Não estou tocando nada no **momento**')
-                    .setColor(process.env.ERR_COLOR)
-                )
+                if (guildQueue) {
+                    return channel.send(embed
+                        .setTitle('Não há nehuma música depois da atual!')
+                        .setColor(process.env.ERR_COLOR)
+                    )
+                } else {
+                    return channel.send(embed
+                        .setTitle('Não estou tocando nada no **momento**')
+                        .setColor(process.env.ERR_COLOR)
+                    )
+                }
             }
         }
     }
