7bbc041ecaa5b5129c61f5c2f5c70c6d2f0e4655
Pablo Vinicius <123amizadepablo@gmail.com>
2019-06-29 18:21:10 -0300

    rework

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -11,48 +11,47 @@ module.exports = class Play extends Command {
 
     async run({ voiceChannel, channel, guild, author, args }) {
         const trueResult = await this.verifyVoice(guild, channel, author, voiceChannel);
-        if (!trueResult) return;
+        if (trueResult) {
+            const paramUrl = /(?:www\.)?(?:youtu\.be|youtube\.com)/;
+            const search = args.slice(0).join(' ');
+            const embed = new ClientEmbed(author);
 
-        const paramUrl = /(?:www\.)?(?:youtu\.be|youtube\.com)/;
-        const search = args.slice(0).join(' ');
-        const embed = new ClientEmbed(author);
+            if (search) {
+                let result = false;
+                if (search.match(paramUrl)) {
+                    const url = args.find(m => m.match(paramUrl));
+                    result = await this.client.music.utils.getUrlSong(url);
+                } else {
+                    result = await this.client.music.utils.getSongByTitle(Util.escapeMarkdown(search));
+                }
 
-        if (search) {
-            let result = false;
-            if (search.match(paramUrl)) {
-                const url = args.find(m => m.match(paramUrl));
-                result = await this.client.music.utils.getUrlSong(url);
-            } else {
-                result = await this.client.music.utils.getSongByTitle(Util.escapeMarkdown(search));
-            }
-
-            if (result.length) {
-                let queueBreak = this.client.music.module.queue.get(guild.id);
-                try {
-                    this.client.music.module.play(result, guild, voiceChannel, author);
-                    let guildQueue = this.client.music.module.queue.get(guild.id);
-                    if (queueBreak) return;
-                    return this.responseMusic(guildQueue, channel);
-                } catch (err) {
+                if (Array.isArray(result) && result.length) {
+                    let queueBreak = this.client.music.module.queue.get(guild.id);
+                    try {
+                        this.client.music.module.play(result, guild, voiceChannel, author);
+                        let guildQueue = this.client.music.module.queue.get(guild.id);
+                        if (!queueBreak) this.responseMusic(guildQueue, channel);
+                    } catch (err) {
+                        return channel.send(embed
+                            .setTitle('Ocorreu um erro!')
+                            .setDescription(err.message)
+                            .setColor(process.env.ERR_COLOR)
+                        )
+                    }
+                } else {
                     return channel.send(embed
                         .setTitle('Ocorreu um erro!')
-                        .setDescription(err.message)
+                        .setDescription('Não consegui encontrar nenhum resultado para a música inserida!')
                         .setColor(process.env.ERR_COLOR)
                     )
                 }
             } else {
                 return channel.send(embed
                     .setTitle('Ocorreu um erro!')
-                    .setDescription('Não consegui encontrar nenhum resultado para a música inserida!')
+                    .setDescription('Por favor insira o **link** ou o **nome** da música desejada! (Youtube)')
                     .setColor(process.env.ERR_COLOR)
                 )
             }
-        } else {
-            return channel.send(embed
-                .setTitle('Ocorreu um erro!')
-                .setDescription('Por favor insira o **link** ou o **nome** da música desejada! (Youtube)')
-                .setColor(process.env.ERR_COLOR)
-            )
         }
     }
 
@@ -67,9 +66,8 @@ module.exports = class Play extends Command {
         queue.on('start', (s) => send(embed(s.addedBy, `Começando a tocar: **[${s.name}](${s.url})** \`[${s.durationContent}]\``)).then((m) => queue.setLastMesage(m)));
         queue.on('error', (s) => send(embed(s.addedBy, `Ocorreu um erro ao tentar reproduzir a música: **[${s.name}](${s.url})**`, true)));
         queue.on('queue', (s, u) => {
-            if (s.length > 1) {
-                send(embed(u, `Adicionei **${s.length}** musicas na queue!`)).then(m => m.delete({ timeout: 30000 }));
-            } else {
+            if (s.length > 1) send(embed(u, `Adicionei **${s.length}** musicas na queue!`));
+            else {
                 send(embed(u, `Adicionei a música **[${s[0].name}](${s[0].url})** na queue!`)).then(m => m.delete({ timeout: 20000 }));
             }
         })
