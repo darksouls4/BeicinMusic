143bd6237ce0e0cc1f4114ceccdfdaecaa35ea25
Pablo Vinicius <123amizadepablo@gmail.com>
2019-06-29 18:20:48 -0300

    update and fix

--- src/commands/music/loop.js
+++ src/commands/music/loop.js
@@ -8,19 +8,22 @@ module.exports = class Loop extends Command {
         })
     }
 
-    async run({ channel, guild, author }) {
-        const embed = new ClientEmbed(author);
-        const guildQueue = await this.client.music.module.queue.get(guild.id);
-        if (guildQueue) {
-            let loopActive = guildQueue.loop ? false : true;
-            return channel.send(embed
-                .setTitle(loopActive ? 'A auto repetição da queue foi **habilitada**!' : 'A auto repetição da queue foi **desabilitada**!')
-            ).then(() => guildQueue.queueLoop(loopActive))
-        } else {
-            return channel.send(embed
-                .setTitle('Não estou tocando nada no **momento**')
-                .setColor(process.env.ERR_COLOR)
-            )
+    async run({ voiceChannel, channel, guild, author }) {
+        const trueResult = await this.verifyVoice(guild, channel, author, voiceChannel);
+        if (trueResult) {
+            const embed = new ClientEmbed(author);
+            const guildQueue = await this.client.music.module.queue.get(guild.id);
+            if (guildQueue) {
+                let loopActive = guildQueue.loop ? false : true;
+                return channel.send(embed
+                    .setTitle(loopActive ? 'A auto repetição da queue foi **habilitada**!' : 'A auto repetição da queue foi **desabilitada**!')
+                ).then(() => guildQueue.queueLoop(loopActive))
+            } else {
+                return channel.send(embed
+                    .setTitle('Não estou tocando nada no **momento**')
+                    .setColor(process.env.ERR_COLOR)
+                )
+            }
         }
     }
 }
