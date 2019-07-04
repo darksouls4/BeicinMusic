90874ce1557216c17b7e04b2cb07656e7004cab7
Pablo Vinicius <123amizadepablo@gmail.com>
2019-07-03 22:08:09 -0300

    nothing more

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
 
-module.exports = new (require('./src/Client.js'))({
-    compress: true,
-    autoReconnect: true
+new (require('./src/Client.js'))({
+    autoReconnect: true,
+    fetchAllMembers: true,
+    disableEveryone: false,
+    restWsBridgeTimeout: 10000,
+    restTimeOffset: 2000,
+    messageCacheMaxSize: 2024,
+    messageCacheLifetime: 1680,
+    messageSweepInterval: 1680,
+    disabledEvents: ['typingStart', 'typingStop', 'guildMemberSpeaking']
 }).login();
